7f4ac4ecf8c3755c3cf2fb018f8c4176f393d75d
boissonnfive <boissonnfive@gmail.com>
2017-09-19 13:50:55 +0200

    Nouveau site de vérification d'adresse externe : api.ipify.org

--- check_network.sh
+++ check_network.sh
@@ -3,23 +3,71 @@
 # ------------------------------------------------------------------------------
 # Auteur: Shaneal Manek + Bruno Boissonnet
 # Date: 27 dec 2006
-# Description: Ce script vérifie si on est connecté au réseau de la maison
+# Description: Vérifie si on est connecté à internet via son FAI
 # Usage:       ./check_network.sh [-h] IP
 # Vérifie si on est connecté à son FAI.
 #   -h:   affiche cette aide
+#	-d:   affiche les informations de debug
 #   IP: adresse IP externe fournie par son FAI
-# Retour: 0 si IP est l'adresse externe, 1 sinon.
+# Retour: 0 : IP est l'adresse externe fourni par le FAI
+#         1 : nombre de paramètres incorrect.
+#         2 : option incorrecte
+#         3 : mauvaise adresse IP ou connexion réseau impossible 
 # Remarques:
-# Nécessite les outils suivants: ifconfig, grep, awk, lynx, sed
-# Variables à modifier:
-#  - WHATISMYIP_ADDRESS: site web affichant l'adresse internet externe
+# Utilise le site https://api.ipify.org (à modifier si besoin)
 # ------------------------------------------------------------------------------
 
-# Codes d'erreur
-# 0: pas d'erreur
-# 1: nombre de paramètres incorrect
-# 2: option incorrecte
-# 3: mauvaise adresse IP ou connexion réseau impossible
+DEBUG=False
+
+main()
+{
+	declare -i RET=0
+	
+	if [ $# -eq 1 ]; then
+		if [[ $1 == -* ]]; then
+			if [ $1 == "-h" ]; then
+				usage
+			else
+				echo "`basename $0`: illegal option $1"
+				usage
+				RET=2
+			fi
+		elif checkNetwork "$1" ; then
+			echo $1
+		else
+			RET=$?
+		fi
+	elif [ $# -eq 2 ]; then
+		if [[ $1 == -* ]]; then
+			if [ $1 == "-h" ]; then
+				usage
+				RET=1
+			elif [ $1 == "-d" ]; then
+				DEBUG=True
+				echo "`basename $0`: mode debug"
+				if checkNetwork "$2" ; then
+					echo $2
+				else
+					RET=$?
+				fi
+			else
+				echo "`basename $0`: illegal option $1"
+				usage
+				RET=2
+			fi
+		else			
+			echo "`basename $0`: illegal option $1"
+			usage
+			RET=2
+		fi
+	else
+		usage
+		RET=1
+	fi
+
+	return $RET
+}
+
 
 # usage
 # Affiche l'aide
@@ -27,19 +75,20 @@
 # retour: toujours 0
 usage()
 {
-	echo "Usage: `basename $0` [-h] IP"
+	echo "Usage: `basename $0` [-hd] IP"
 	echo "Vérifie si on est connecté à son FAI. "
 	echo "  -h:   affiche cette aide"
+	echo "  -d:   affiche les informations de debug"
 	echo "  IP: adresse IP externe fournie par son FAI"
 }
 
 # checkNetwork
 # vérifie si on est connecté au réseau de la maison
 # $1: adresse IP fournie par mon FAI (IP externe)
-# return: 0 si OK, 1 sinon
+# return: 0 si OK, 3 si mauvaise adresse IP ou connexion réseau impossible
 checkNetwork()
 {
-	local -r WHATISMYIP_ADDRESS="http://www.whatismyip.fr/"
+	local -r WEB_SITE="https://api.ipify.org"
 	local -i RET=3
 
 	# Récupère les status wifi et ethernet
@@ -47,52 +96,40 @@ checkNetwork()
 	# 2- Sélectionne la ligne où il y a le mot "status"
 	# 3- Affiche le dernier mot de cette ligne ("active" ou "inactive")
 	status_wifi=$(ifconfig en1 | grep status | awk '{print $NF}')
+	# ou :        ifconfig en0 | sed -nE 's/^.+status: ([a-z]+)$/\1/p'
+	# ou encore : ifconfig en0 | sed -nE 's/[^:]+:[[:space:]]([[:alpha:]]+)$/\1/p'
+	if [ $DEBUG == "True" ]; then
+		echo "WIFI : "$status_wifi
+	fi
+	
 	status_ethernet=$(ifconfig en0 | grep status | awk '{ print $NF }')
+	if [ $DEBUG == "True" ]; then
+		echo "Ethernet : "$status_ethernet
+	fi
 
-	# Récupère l'adresse IP externe de la machine
-	# 1- Récupère les infos sur la page whatismyip
-	# 2- Sélectionne la ligne où il y a le mot "Adresse IP :" et
-	#    remplace tout ce qu'il y a avant l'adresse IP par rien
+	# Récupère l'adresse IP externe de la machine si on est connecté
 	if [ "$status_wifi" == "active" ] || [ "$status_ethernet" == "active" ]; then
-		IP=`/usr/local/bin/lynx -dump "${WHATISMYIP_ADDRESS}" 2> /dev/null \
-			| sed -n '/Adresse IP :/s/^[ ]*Adresse IP : //p'`
+		IP=$(curl -s ${WEB_SITE})
+		if [ $DEBUG == "True" ]; then
+			echo "Adresse IP : "$IP
+		fi
+		if [ "${IP}" == "$1" ]; then
+			# echo "Connexion réseau maison(${IP}) via WIFI: OK"
+			RET=0
+		fi
+		# else : mauvaise adresse IP 
 	fi
-
-	if [ "$status_wifi" == "active" ] && [ "${IP}" == "$1" ]; then
-		# echo "Connexion réseau maison(${IP}) via WIFI: OK"
-		RET=0
-	elif [ "$status_ethernet" == "active" ] && [ "${IP}" == "$1" ]; then
-		# echo "Connexion réseau maison(${IP}) via ethernet: OK"
-		RET=0
-	fi
+	# else : connexion réseau impossible
 
 	return $RET
 }
 
-declare -i RET=0
 
-if [ $# -eq 1 ]; then
-	if [[ $1 == -* ]]; then
-		if [ $1 == "-h" ]; then
-			usage
-		else
-			echo "`basename $0`: illegal option $1"
-			usage
-			RET=2
-		fi
-	elif checkNetwork "$1" ; then
-		echo $1
-	else
-		RET=$?
-	fi
-else
-	usage
-	RET=1
-fi
+# ------------------------
+#    DÉBUT DU PROGRAMME
+# ------------------------
 
-
-
-exit $RET
+main $@
 
 # -----
 # Tests
@@ -104,34 +141,49 @@ exit $RET
 # 0
 
 # $ ./check_network.sh -h
-# Usage: check_network.sh [-h] IP
+# Usage: check_network.sh [-hd] IP
 # Vérifie si on est connecté à son FAI. 
 #   -h:   affiche cette aide
+#   -d:   affiche les informations de debug
 #   IP: adresse IP externe fournie par son FAI
 # $ echo $?
 # 0
 
 # $ ./check_network.sh
-# Usage: check_network.sh [-h] IP
+# Usage: check_network.sh [-hd] IP
 # Vérifie si on est connecté à son FAI. 
 #   -h:   affiche cette aide
+#   -d:   affiche les informations de debug
 #   IP: adresse IP externe fournie par son FAI
 # $ echo $?
 # 1
 
 # $ ./check_network.sh -h 88.162.114.99
-# Usage: check_network.sh [-h] IP
+# Usage: check_network.sh [-hd] IP
 # Vérifie si on est connecté à son FAI. 
 #   -h:   affiche cette aide
+#   -d:   affiche les informations de debug
 #   IP: adresse IP externe fournie par son FAI
 # $ echo $?
 # 1
 
-# $ ./check_network.sh -hg
-# check_network.sh: illegal option -hg
-# Usage: check_network.sh [-h] IP
+# $ ./check_network.sh -hd
+# check_network.sh: illegal option -hd
+# Usage: check_network.sh [-hd] IP
 # Vérifie si on est connecté à son FAI. 
 #   -h:   affiche cette aide
+#   -d:   affiche les informations de debug
+#   IP: adresse IP externe fournie par son FAI
+# $ echo $?
+# 2
+
+
+# $ ./check_network.sh -d
+# check_network.sh: illegal option -d
+# Usage: check_network.sh [-hd] IP
+# Vérifie si on est connecté à son FAI. 
+#   -h:   affiche cette aide
+#   -d:   affiche les informations de debug
 #   IP: adresse IP externe fournie par son FAI
 # $ echo $?
 # 2
